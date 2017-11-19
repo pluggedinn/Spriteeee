f5c661eb782fed2e551822fd2c5f8de2cbe37cfc
sonsini <sonsini@lab2-13.eng.utah.edu>
2017-11-18 17:51:09 -0700

    added icons

--- Spriteee.pro
+++ Spriteee.pro
@@ -37,5 +37,3 @@ HEADERS += \
 
 FORMS += \
         mainwindow.ui
-
-RESOURCES += \
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,6 +18,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
     numFrames = 0;
     createEmptyFrame();
 
